26af0b49eb8a4c2ce2aee9a0beaa113ca4af82c7
ILWEL ISAAC <ilwelisaac@hotmail.com>
2023-11-20 16:57:43 -0300

    feat: set project

---
+++ main/background.ts
@@ -0,0 +1,49 @@
+import path from 'path'
+import { app, ipcMain } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+;(async () => {
+  await app.whenReady()
+
+  const mainWindow = createWindow('main', {
+    width: 400,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    frame: false,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#333333',
+      symbolColor: '#9D8062',
+      height: 50
+    },
+    maximizable: false,
+    resizable: false
+  })
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}`)
+    mainWindow.webContents.openDevTools()
+  }
+})()
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('message', async (event, arg) => {
+  event.reply('message', `${arg} World!`)
+})
---
+++ renderer/pages/_app.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import Head from 'next/head'
+import logo from '../public/images/logo.svg'
+import Image from 'next/image'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Floating</title>
+      </Head>
+      <div className='box-border text-text bg-base h-screen'>
+        <div className='header min-h-[50px] max-h-[50px] p-3 select-none flex gap-2'>
+          <Image src={logo} alt='gci logo' height={20} width={20}/>
+          <p >
+            Floating
+          </p>
+        </div>      
+        <main className='flex h-[84%] items-center justify-center'>
+          <Component {...pageProps} />
+        </main>  
+
+      </div>
+    </>
+  )
+}
+
+export default MyApp
