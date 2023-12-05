b99f762f54603cb67093750def2d061410122f93
ILWEL ISAAC <ilwelisaac@hotmail.com>
2023-12-05 15:37:39 -0300

    feat: api service list vehicles

--- renderer/services/ApiService.ts
+++ renderer/services/ApiService.ts
@@ -16,4 +16,19 @@ export class ApiService{
     }
 
   }
+
+  public static async listVehicles(token:string, plate: string){
+    
+    const options = {
+      headers: { authorization: (token as string) }
+    }
+
+    const res = await fetch(`${this.api}/veiculos?_search=false&nd=1698927680302&rows=15&page=1&sidx=id&sord=asc&placa=${plate}`, options)
+
+    if(res.ok){
+      const data = await res.json()
+      return data.rows
+    }
+  
+  }
 }
