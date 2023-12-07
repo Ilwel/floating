4cfd2da5a25906bba440bdb616bdc146a4942362
ILWEL ISAAC <ilwelisaac@hotmail.com>
2023-12-07 17:21:46 -0300

    feat: list 5 vehicles

--- renderer/services/ApiService.ts
+++ renderer/services/ApiService.ts
@@ -23,7 +23,7 @@ export class ApiService{
       headers: { authorization: (token as string) }
     }
 
-    const res = await fetch(`${this.api}/veiculos?_search=false&nd=1698927680302&rows=15&page=1&sidx=id&sord=asc&placa=${plate}`, options)
+    const res = await fetch(`${this.api}/veiculos?_search=false&nd=1698927680302&rows=5&page=1&sidx=id&sord=asc&placa=${plate}`, options)
 
     if(res.ok){
       const data = await res.json()
